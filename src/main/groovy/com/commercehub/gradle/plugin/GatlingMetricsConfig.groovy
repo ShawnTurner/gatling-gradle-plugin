6ded4a649e4c284139dcb8a2f646feff2acc1c82
bmanley <bmanley@commercehub.com>
2016-09-08 11:39:33 -0400

    Added copyright message to all source files.

--- src/main/groovy/com/commercehub/gradle/plugin/GatlingMetricsConfig.groovy
+++ src/main/groovy/com/commercehub/gradle/plugin/GatlingMetricsConfig.groovy
@@ -1,5 +1,20 @@
-package com.commercehub.gradle.plugin
+/*
+ * Copyright (C) 2016 Commerce Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.commercehub.gradle.plugin
 
 class GatlingMetricsConfig {
     public static final String DEFAULT_METRIC_PREFIX = ''
@@ -7,13 +22,13 @@ class GatlingMetricsConfig {
     public static final Number DEFAULT_DEGRADATION_TOLERANCE = 0.50
 
     String graphiteUrl
-    
+
     String metricPrefix
-    
+
     String getMetricPrefix() {
         metricPrefix ?:  DEFAULT_METRIC_PREFIX
     }
-    
+
     Integer daysToCheck
 
     int getDaysToCheck() {
--- src/main/groovy/com/commercehub/gradle/plugin/GatlingTask.groovy
+++ src/main/groovy/com/commercehub/gradle/plugin/GatlingTask.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Commerce Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.commercehub.gradle.plugin
 
 import org.gradle.api.DefaultTask
@@ -7,9 +23,6 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.process.JavaForkOptions
 import org.gradle.process.internal.DefaultJavaForkOptions
 
-/**
- * Created by bmanley on 11/16/15.
- */
 class GatlingTask extends DefaultTask {
     private final GatlingPluginExtension extension =
             project.extensions[GatlingPlugin.GATLING_EXTENSION_NAME] as GatlingPluginExtension
