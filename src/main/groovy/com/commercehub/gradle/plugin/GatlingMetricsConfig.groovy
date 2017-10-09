cf53bcb19dbf9bbdbbaf4a3a0235d6b7be0bbb7d
bmanley <bmanley@commercehub.com>
2017-10-09 17:54:54 -0400

    Updated copyright date.

--- src/main/groovy/com/commercehub/gradle/plugin/GatlingMetricsConfig.groovy
+++ src/main/groovy/com/commercehub/gradle/plugin/GatlingMetricsConfig.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Commerce Technologies, LLC
+ * Copyright (C) 2017 Commerce Technologies, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
