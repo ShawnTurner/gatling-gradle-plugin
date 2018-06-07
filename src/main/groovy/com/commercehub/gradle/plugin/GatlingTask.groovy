e9216f5655179194a1ebb579c75086196088a669
bmanley <bmanley@commercehub.com>
2018-06-07 11:11:55 -0400

    Using new GatlingTaskMetricsConfig.

--- src/main/groovy/com/commercehub/gradle/plugin/GatlingTask.groovy
+++ src/main/groovy/com/commercehub/gradle/plugin/GatlingTask.groovy
@@ -165,40 +165,4 @@ class GatlingTask extends DefaultTask {
         }
         project.logger.error(message)
     }
-
-    class GatlingTaskMetricsConfig {
-        private final GatlingPluginExtension extension
-
-        GatlingTaskMetricsConfig(GatlingPluginExtension extension) {
-            this.extension = extension
-        }
-
-        def metricsToCheck = []
-
-        String graphiteUrl
-
-        String getGraphiteUrl() {
-            graphiteUrl ?: extension.metrics.graphiteUrl
-        }
-
-        String metricPrefix
-
-        String getMetricPrefix() {
-            metricPrefix ?:  extension.metrics.metricPrefix
-        }
-
-        Integer daysToCheck
-
-        int getDaysToCheck() {
-            daysToCheck ?: extension.metrics.daysToCheck
-        }
-
-        Number degradationTolerance
-
-        Number getDegradationTolerance() {
-            degradationTolerance ?: extension.metrics.degradationTolerance
-        }
-
-        def thresholdsByMetricIndex = []
-    }
 }
